bbc3c47425887ff2eff2c06b7ba2ad68daa3c276
phreshbrread <crustydog9@gmail.com>
2022-10-02 03:27:28 +0000

    Add files via upload

--- src/Program.cs
+++ src/Program.cs
@@ -1,53 +1,43 @@
-﻿using System;
-using System.IO;
-
-namespace create_file
+﻿static void InvalidUsage()
 {
-    internal class Program
-    {
-
-        static void InvalidUsage()
-        {
-            Console.WriteLine("Usage: create-file [size] [unit] [path/filename]");
-        }
-        static void Main(string[] args)
-        {
-            if (args.Length != 3)
-            {
-                InvalidUsage();
-                return;
-            }
-
-            long size;
-            long.TryParse(args[0], out size);
-            string unit = args[1].ToLower();
-            string path = args[2];
-
-            Console.WriteLine(size + " " + unit + " " + path);
-
-            switch (unit)
-            {
-                case "b":
-                    break;
-                case "kb":
-                    size = size * 1024;
-                    break;
-                case "mb":
-                    size = size * 1048576;
-                    break;
-                case "gb":
-                    size = size * 1073741824;
-                    break;
-                case "tb":
-                    size = size * 1099511627776;
-                    break;
-                default:
-                    InvalidUsage();
-                    break;
-            }
-
-            var fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
-            fileStream.SetLength(size);
-        }
-    }
+    Console.WriteLine("Usage: create-file [size] [unit] [path/filename]");
 }
+
+// ----- //
+
+if (args.Length != 3)
+{
+    InvalidUsage();
+    return;
+}
+
+long size;
+long.TryParse(args[0], out size);
+string unit = args[1].ToLower();
+string path = args[2];
+
+Console.WriteLine(size + " " + unit + " " + path);
+
+switch (unit)
+{
+    case "b":
+        break;
+    case "kb":
+        size = size * 1024;
+        break;
+    case "mb":
+        size = size * 1048576;
+        break;
+    case "gb":
+        size = size * 1073741824;
+        break;
+    case "tb":
+        size = size * 1099511627776;
+        break;
+    default:
+        InvalidUsage();
+        break;
+}
+
+var fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+fileStream.SetLength(size);
