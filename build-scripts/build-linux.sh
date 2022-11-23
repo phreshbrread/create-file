c172ce6752c88bfdae480f4ede4ead09427df8f5
phreshbrread <crustydog9@gmail.com>
2022-11-24 10:23:01 +1100

    Add files via upload

--- build-scripts/build-linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet publish --configuration Release --runtime linux-x64 --no-self-contained /p:PublishSingleFile=true
--- build-scripts/build-win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet publish --configuration Release --runtime win-x64 --no-self-contained /p:PublishSingleFile=true
